b780a89863386054451f5d34d7a0900f99ae5ab0
snehamali43342-sudo <snehamali43342@gmail.com>
2026-02-03 11:12:10 +0530

    Create exp1activity.py

---
+++ exp1/exp1activity.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  3 10:40:43 2026
+
+@author: User
+"""
+
+num1 =int(input("Enter first number:"))
+num2 =int(input("Enter second number:"))
+
+sum=num1+num2
+print("Addition=" ,sum)
+
+sub=num1-num2
+print("substraction=" , sub)
+
+mul=num1*num2
+print("Multiplication=" , mul)
+
+div=num1/num2
+%runfile C:/Users/User/untitled0.py --wdir
+Enter first number:2
+Enter second number:3
+Addition= 5
+substraction= -1
+Multiplication= 6
+division= 0.6666666666666666print("division=" , div)
