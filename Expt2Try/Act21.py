885f613d36ff645d50910e2600e312c9ebcf497b
snehamali43342-sudo <snehamali43342@gmail.com>
2026-02-10 11:13:13 +0530

    Add files via upload

---
+++ Expt2Try/Act21.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 10:28:10 2026
+
+@author: User
+"""
+
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("Eligible for movie hall entry.")
+else: 
+    print("Not eligible for movie hall entry.")
+    
