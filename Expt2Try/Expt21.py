10e70aabdf0e19f04b0fc381219d1a1f5f808da5
snehamali43342-sudo <snehamali43342@gmail.com>
2026-02-10 11:09:56 +0530

    Create Expt21.py

---
+++ Expt2Try/Expt21.py
@@ -0,0 +1,12 @@
+# Program To Check Leap Year
+"""
+Created on Tue Feb 10 10:01:14 2026
+
+@author: Sneha Mali
+"""
+
+year = int(input("Enter year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
