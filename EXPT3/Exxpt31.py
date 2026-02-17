9ec147b88bb0c436996f5c92367193f9ff9e39c9
snehamali43342-sudo <snehamali43342@gmail.com>
2026-02-17 10:17:27 +0530

    Add pattern printing program in Exxpt31.py
    Initial implementation of a pattern printing program.

---
+++ EXPT3/Exxpt31.py
@@ -0,0 +1,13 @@
+# program to print pattern 
+"""
+Created on Tue Feb 17 09:33:48 2026
+
+@author: sneha mali
+"""
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
